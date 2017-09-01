992929a2b00e2f25253ba3385f30ee799fbfa135
wueato03 <alvaro.torres@entelgy.com>
2017-09-01 13:14:02 +0200

    animated Lemmings

--- Lemming.py
+++ Lemming.py
@@ -25,6 +25,11 @@ class Lemming:
         self.vel = 0.03, 0
         self.accel = 0, 0
         self.sprite = Sprite()
+        self.totalImages = 7
+        self.imagePointer = 0
+        self.image = self.getNextImage()
+        self.times = 0
+        self.period = 10
     def draw(self,t, screen):
         # print(self.pos)
         self.vel = self.vel[0] + 0.5 * self.accel[0] * t*t, self.vel[1] + 0.5 * self.accel[1] * t*t
@@ -33,16 +38,23 @@ class Lemming:
         self.sprite.rect.x = self.pos[0]
         self.sprite.rect.y = self.pos[1]
 
-        screen.blit(self.sprite.image, self.rect, self.getLemming(0,2))
+        self.times += 1
+        if self.times > self.period:
+            self.image = self.getNextImage()
+            self.times = 0
+        screen.blit(self.sprite.image, self.rect, self.image)
 
 
         # self.color = 50, 0, 0
 
         # pygame.draw.rect(screen, (0, 100, 0), self.rect, 2)
 
-    def getLemming(self,x,y):
-        side = 47
-        return (side*x+10, side* y+-1, side*0.6, side)
+    def getNextImage(self):
+        side = 50
+        self.imagePointer += 1
+        if self.imagePointer >= self.totalImages:
+            self.imagePointer = 0
+        return side*self.imagePointer, side * 1.9-5, side, side-10
 
 
 
